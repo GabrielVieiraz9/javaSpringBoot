667860bac2d07b0bc2f123ccf71ce264d3707dba
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-03-14 15:15:38 -0300

    Atividade prática – Desenvolvendo o Backend com Spring (Loja de Games)

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,79 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome do produto é obrigatório!")
+	@Size(min = 5, max = 100, message = "O nome do produto deve conter entre 05 e 100 caracteres")
+	private String nome;
+	
+    @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero")
+    @Digits(integer = 6, fraction = 2, message = "O preço deve ter no máximo 6 dígitos inteiros e 2 dígitos decimais")
+	private BigDecimal preco;
+	
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto") // fazer com que a categoria não chame o produto e fique num ciclo infinito de categoria:produto e produto:categoria
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
